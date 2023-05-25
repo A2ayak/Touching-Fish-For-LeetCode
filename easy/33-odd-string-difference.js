8be3c7592444fa8265e0a50b3532d0933c6b521d
A2ayak <429853312@qq.com>
2023-05-25 18:13:03 +0800

    2023-5-25：哈希表

---
+++ easy/33-odd-string-difference.js
@@ -0,0 +1,18 @@
+const { log } = require('console')
+
+/**
+ * 差值数组不同的字符串
+ * https://leetcode.cn/problems/odd-string-difference/
+ * @param {string[]} words
+ * @return {string}
+ **/
+var oddString = function (words) {
+  // const obj = {}
+  // new Array(26).fill(0).map((i, idx) => {
+  //   obj[String.fromCharCode(97 + idx)] = idx
+  // })
+}
+
+const words = ['aaa', 'bob', 'ccc', 'ddd'] // 'bob'
+
+console.log(oddString(words))
