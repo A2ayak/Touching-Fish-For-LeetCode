33cb37ead378f877d0404724bbba0837016f0a58
A2ayak <429853312@qq.com>
2024-08-25 22:08:24 +0800

    2024-08-25

--- easy/45-merge-sorted-array.js
+++ easy/45-merge-sorted-array.js
@@ -21,31 +21,31 @@ var merge = function (nums1, m, nums2, n) {
   // }
   // nums1.sort((a, b) => a - b)
   /* 法二 */
-  // nums1.splice(m, nums1.length - m, ...nums2)
-  // nums1.sort((a, b) => a - b)
-  // console.log(nums1)
+  nums1.splice(m, nums1.length - m, ...nums2)
+  nums1.sort((a, b) => a - b)
+  console.log(nums1)
   /* 法三-双指针 */
-  let i = 0,
-    j = 0
-  const n1 = nums1.length
-  const n2 = nums2.length
-  const res = []
-  while (i < n1 && j < n2) {
-    if (nums1[i] >= nums2[j]) {
-      res.push(nums2[j++])
-    } else {
-      res.push(nums1[i++])
-    }
-  }
-  while (i < n1) {
-    res.push(nums1[i++])
-  }
-  while (j < n2) {
-    res.push(nums2[j++])
-  }
+//   let i = 0,
+//     j = 0
+//   const n1 = nums1.length
+//   const n2 = nums2.length
+//   const res = []
+//   while (i < n1 && j < n2) {
+//     if (nums1[i] >= nums2[j]) {
+//       res.push(nums2[j++])
+//     } else {
+//       res.push(nums1[i++])
+//     }
+//   }
+//   while (i < n1) {
+//     res.push(nums1[i++])
+//   }
+//   while (j < n2) {
+//     res.push(nums2[j++])
+//   }
 
-  return res
-}
+//   return res
+// }
 const nums1 = [1, 2, 3],
   m = 3,
   nums2 = [2, 5, 6],
